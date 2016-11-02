9e2e618d5854bd36b38a9eb110981095c29798ed
arituerto <aleritu@gmail.com>
2016-11-01 17:15:02 -0700

    Added squeleton functions for camera

--- app/src/main/java/arituerto/sensorandcameralogger/MainActivity.java
+++ app/src/main/java/arituerto/sensorandcameralogger/MainActivity.java
@@ -1,21 +1,38 @@
 package arituerto.sensorandcameralogger;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureFailure;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +40,10 @@ import android.widget.ProgressBar;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -49,14 +69,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Map<Sensor, Logger> mSensorLoggerMap;
 
     // CAMERA
-    private CameraManager mCameraManager;
-    private String mCameraId;
-    private CameraDevice mCameraDevice;
-    private CameraCharacteristics mCameraCharacteristics;
+    private String cameraId;
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest captureRequest;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
 
     // Logging data
-    private boolean mLoggingActive;
-    private boolean mCameraLogging;
+    private boolean mLoggingActive = false;
+    private boolean mCameraLogging = false;
     private File loggingDir;
     private File imageDir;
     private String dataSetName;
@@ -87,30 +112,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         // CAMERA
-        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        String[] cameraIds;
-        try{
-            cameraIds = mCameraManager.getCameraIdList();
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-            cameraIds = null;
-        }
-        if (cameraIds == null) {
-            Log.i(TAG, "Camera not available");
-            mCameraLogging = false;
-        } else {
-            mCameraLogging = true;
-            for (int iCamera = 0; iCamera < cameraIds.length; iCamera++) {
-                try {
-                    mCameraCharacteristics = mCameraManager.getCameraCharacteristics(cameraIds[iCamera]);
-                    if (mCameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
-                        mCameraId = cameraIds[iCamera];
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        startBackgroundThread();
+        openCamera();
 
         //VISUAL
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -127,6 +130,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
+    protected void onDestroy() {
+
+        Log.i(TAG, " onDestroy");
+
+        super.onDestroy();
+        closeCamera();
+        stopBackgroundThread();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
     if (requestCode == SENSORS_SETTINGS_REQUEST) {
         if (resultCode == RESULT_OK) {
@@ -138,6 +151,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    // SENSORS FUNCTIONS
     private void startSensorListeners() {
         for (int iSensor = 0; iSensor < mSelectedSensorList.length; iSensor++) {
             if (mSelectedSensorList[iSensor]) {
@@ -154,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Log.i(TAG, "Sensor Listeners OFF");
     }
 
-    private void startLogging() {
+    private void startSensorsLogging() {
 
         startSensorListeners();
 
@@ -164,26 +178,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HHmmss");
         String currentDateAndTime = sdf.format(new Date());
 
-        // Create directory
+        // CREATE LOGGING DIRECTORY
         loggingDir = new File(Environment.getExternalStorageDirectory().getPath() +
                 "/" + currentDateAndTime +
                 "_" + Build.MANUFACTURER +
                 "_" + Build.MODEL +
                 "_" + dataSetName);
-
         try {
             loggingDir.mkdirs();
         } catch (SecurityException e) {
             e.printStackTrace();
         }
 
-        imageDir = new File(loggingDir.getPath() + "/images");
-        try {
-            imageDir.mkdirs();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
+//        // CREATE IMAGES DIRECTORY
+//        imageDir = new File(loggingDir.getPath() + "/images");
+//        if (mCameraLogging) {
+//            try {
+//                imageDir.mkdirs();
+//            } catch (SecurityException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
+        // CREATE SENSOR LOGGERS
         String loggerFileName;
         for (int iSensor = 0; iSensor < mSelectedSensorList.length; iSensor++ ) {
             if (mSelectedSensorList[iSensor]) {
@@ -212,7 +229,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
-    private void stopLogging() {
+    private void stopSensorsLogging() {
         for (Map.Entry<Sensor, Logger> iSensorLogger : mSensorLoggerMap.entrySet()) {
             try {
                 iSensorLogger.getValue().close();
@@ -244,12 +261,179 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
+    // CAMERA FUNCTIONS
+
+    protected void startBackgroundThread() {
+
+        Log.i(TAG, "Camera Background thread Started");
+
+        mBackgroundThread = new HandlerThread("cameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    protected void stopBackgroundThread() {
+
+        Log.i(TAG, "Camera Background thread Stopped");
+
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            Log.e(TAG, "Camera Device Opened");
+            cameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            Log.e(TAG, "Camera Device Disconnected");
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onClosed(CameraDevice camera) {
+            Log.e(TAG, "Camera Device Closed");
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            Log.e(TAG, "Camera Device Closed because error");
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    };
+
+    private CameraCaptureSession.CaptureCallback captureSessionCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+
+        @Override
+        public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            Log.i(TAG, "onCaptureCompleted");
+        }
+
+        @Override
+        public void onCaptureFailed(CameraCaptureSession session, CaptureRequest request, CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+            Log.i(TAG, "onCaptureFailed");
+        }
+    };
+
+    private CameraCaptureSession.StateCallback captureSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            try {
+                session.capture(captureRequestBuilder.build(), captureSessionCaptureCallback, mBackgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+        }
+    };
+
+    protected void startCameraAcquisition() {
+
+        if(null == cameraDevice) {
+            Log.e(TAG, "cameraDevice is null");
+            return;
+        }
+
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+
+            Size[] jpegSizes = null;
+            if (characteristics != null) {
+                jpegSizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP).getOutputSizes(ImageFormat.JPEG);
+            }
+            int width = 640;
+            int height = 480;
+            if (jpegSizes != null && 0 < jpegSizes.length) {
+                width = jpegSizes[0].getWidth();
+                height = jpegSizes[0].getHeight();
+            }
+
+            ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            List<Surface> outputSurfaces = new ArrayList<Surface>();
+            outputSurfaces.add(reader.getSurface());
+
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+
+            captureRequestBuilder.addTarget(reader.getSurface());
+
+            captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            ImageReader.OnImageAvailableListener readerListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Log.i(TAG,"onImageAvailable");
+                }
+            };
+
+            reader.setOnImageAvailableListener(readerListener, mBackgroundHandler);
+
+            cameraDevice.createCaptureSession(outputSurfaces, captureSessionStateCallback, mBackgroundHandler);
+
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openCamera() {
+
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            cameraId = manager.getCameraIdList()[0];
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            assert map != null;
+            imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
+            // Add permission for camera and let user grant the permission
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+                return;
+            }
+            manager.openCamera(cameraId, cameraStateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        Log.i(TAG, "openCamera");
+    }
+
+    private void closeCamera() {
+        if (null != cameraDevice) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+        if (null != imageReader) {
+            imageReader.close();
+            imageReader = null;
+        }
+    }
+
+    // BUTTONS FUNCTIONS
     private View.OnClickListener startClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             if (!mLoggingActive) {
                 Log.i(TAG, "Start Logging");
-                startLogging();
+                startSensorsLogging();
+                startCameraAcquisition();
                 mLoggingActive = true;
                 progressBar.setVisibility(View.VISIBLE);
             } else {
@@ -263,7 +447,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         public void onClick(View v) {
             if (mLoggingActive) {
                 Log.i(TAG, "Stop Logging");
-                stopLogging();
+                stopSensorsLogging();
                 mLoggingActive = false;
                 progressBar.setVisibility(View.GONE);
             } else {
